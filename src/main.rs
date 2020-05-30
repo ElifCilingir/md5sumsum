3b9e049caab7bcd2a740f7af1a06c3176ed5ba4d
Elif C <elif.cilingir061@gmail.com>
2020-05-30 21:02:58 +0200

    little optimization

--- src/main.rs
+++ src/main.rs
@@ -8,10 +8,7 @@ fn main() {
     let mut hashcat = String::from("");
     for _argument in env::args().skip(1) {
 
-        for entry in WalkDir::new(_argument)
-                .into_iter()
-                .filter_map(Result::ok)
-                .filter(|e| !e.file_type().is_dir())
+        for entry in WalkDir::new(_argument).into_iter().filter_map(Result::ok).filter(|e| !e.file_type().is_dir())
         {
             paths.push(String::from(entry.path().to_string_lossy()))
         }
@@ -22,7 +19,7 @@ fn main() {
         cmd.arg("-c").arg(format!("md5sum \"{}\"", paths[i]));
         let output = String::from_utf8_lossy(&cmd.output().unwrap().stdout).to_string();
         
-        let vec: Vec<&str> = output.split(" ").collect();
+        let vec: Vec<&str> = output.split(' ').collect();
         hashcat += vec[0];
     }
     let mut cmd = Command::new("sh");
